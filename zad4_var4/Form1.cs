aaeb9d1e3add712442750ea6640b764f7ccb9f89
katya1331 <152582001+katya1331@users.noreply.github.com>
2024-02-25 21:48:15 +0500

    Add files via upload

---
+++ zad4_var4/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using zad4_var4.Properties;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zad4_var4
+{
+    public partial class Form1 :Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+
+        }
+        Room room1 = new Room();
+        private void button1_Click (object sender, EventArgs e)
+        {
+            room1.s1 = $"Площадь комнаты { room1.S(room1.x, room1.y)} метров квадратных";
+            label1.Text = room1.s1;
+        }
+
+        private void button2_Click (object sender, EventArgs e)
+        {
+            room1.s2 = $"Объем комнаты { room1.V(room1.x, room1.y, room1.z)} метров кубических";
+            label1.Text = room1.s2;
+        }
+
+        private void button4_Click (object sender, EventArgs e)
+        {
+           
+            room1.s4 = room1.Oboi(room1.x, room1.y, room1.z,  room1.CountWindow,  room1.xWindow,  room1.yWindow,  room1.xdoor,  room1.ydoor);
+            label1.Text = room1.s4;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           try
+           {
+                
+                room1.x = Double.Parse(textBox1.Text);
+                room1.y = Double.Parse(textBox2.Text);
+                room1.z = Double.Parse(textBox3.Text);
+                room1.CountWindow = Int32.Parse(textBox4.Text);
+                room1.xWindow = Double.Parse(textBox5.Text);
+                room1.yWindow = Double.Parse(textBox6.Text);
+                room1.xdoor = Double.Parse(textBox7.Text);
+                room1.ydoor = Double.Parse(textBox8.Text);
+                    button5.Enabled = false;
+           }
+           catch
+           {
+                    MessageBox.Show("Все поляны должны быть заполнены цифрами");
+           }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            list.Add("x:"+Convert.ToString(room1.x));
+            list.Add("y:" + Convert.ToString(room1.y));
+            list.Add("z:" + Convert.ToString(room1.z));
+            list.Add("CountWindow:" + Convert.ToString(room1.CountWindow));
+            list.Add("xWindow:" + Convert.ToString(room1.xWindow));
+            list.Add("yWindow:" + Convert.ToString(room1.yWindow));
+            list.Add("xdoor:" + Convert.ToString(room1.xdoor));
+            list.Add("ydoor:" + Convert.ToString(room1.ydoor));
+            list.Add("S:" + Convert.ToString(room1.S(room1.x, room1.y)));
+            list.Add("V:" + Convert.ToString(room1.V(room1.x, room1.y, room1.z)));
+            list.Add(Convert.ToString(room1.s3)); 
+            list.Add(Convert.ToString(room1.s4));
+            File.AppendAllLines("Rooms.txt", list);
+            button5.Enabled = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            room1.s3 = room1.Info(room1.x, room1.y);
+            label1.Text = room1.s3;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            label1.Text="";
+        }
+    }
+}
---
+++ zad4_var4/Properties/Room.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace zad4_var4.Properties
+{
+    internal class Room
+    {
+        public double x {  get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public int CountWindow;
+        public double xWindow;
+        public double yWindow;
+        public double xdoor;
+        public double ydoor;
+        public string s1;
+        public string s2;
+        public string s3;
+        public string s4;
+        public double S (double x, double y)
+        {
+            return x * y;
+        }
+        public double V (double x, double y, double z)
+        {
+            return x * y * z;
+        }
+        public double P (double x, double y)
+        {
+            return (x + y)*2;
+        }
+        public string Oboi(double x, double y, double z, int CountWindow, double xWindow,double yWindow,double xdoor,double ydoor)
+        {
+            double Sremonta = (z * P(x, y)) - (CountWindow * P(xWindow, yWindow)) - P(xdoor, ydoor);
+            double nado1 = Math.Ceiling(Sremonta / 15);
+            double nado2 = Math.Ceiling(Sremonta / 10);
+            if (nado2<1)
+            {
+                s4 = $"Вам надо хватит и 1 рулона в 10м";
+            }else
+            s4 = $"Вам надо рулонов обоев по 15м - {nado1}  \n Вам надо рулонов обоев по 10м -{nado2}";
+            return s4;
+        }
+        public string Info(double x, double y)
+        {
+            double people = Math.Floor((x * y) / 10);
+            if(people > 0)
+            {
+                s3 = $"Вы можете поселить - {Math.Floor((x * y) / 10)} человек(а) в комнату";
+            }else
+            s3 = $"Комната будет тесной даже для одного человека";
+            return s3;
+        }
+    }
+}
